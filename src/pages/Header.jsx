8f0f19139d42fdbe46d3e9ad3323fa7ba12dc3dc
Olawoyin <yustee2017@gmail.com>
2024-12-17 21:36:07 +0100

    Header section done

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { LuPizza } from "react-icons/lu";
+import { AiOutlineShoppingCart } from "react-icons/ai"; 
+
 
 const Header = () => {
   return (
-    <div>This is the header for the project</div>
-  )
-}
+    <header className="header">
+      <div className="container p-0">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-1 border-bottom">
+          <a
+            href="/"
+            className="d-flex gap-2 align-items-center  mb-2 p-0 mb-md-0 text-dark text-decoration-none"
+          >
+            <LuPizza size={30} />
+            <b>Pizza_Rush</b>
+          </a>
 
-export default Header
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <a href="nac" className="nav-link px-2 ">Home</a>
+            </li>
+            <li>
+              <a href="nav" className="nav-link ">Menu</a>
+            </li>
+            <li>
+              <a href="nav" className="nav-link ">Specials</a>
+            </li>
+            <li>
+              <a href="nav" className="nav-link ">Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="nav" className="nav-link">About Us</a>
+            </li>
+          </ul>
+
+          <div className="col-md-3 d-flex justify-content-end align-items-center gap-3">
+            <li><a href="nav"><AiOutlineShoppingCart size={24} /></a></li>
+            <li>
+              <a href="nav" className="nav-link">Order Online</a>
+            </li>
+            <li>
+              <a href="nav" className="nav-link ">Sign Up</a>
+            </li>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
