a3ad160ed871c7869bf87fe6cb76883ef7240168
Olawoyin <yustee2017@gmail.com>
2024-12-17 20:55:01 +0100

    created a router to swith between pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import SharedLayout from "./components/SharedLayout.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const main = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<SharedLayout />}>
+        <Route
+          index
+          element={
+            <Home />
+          }
+        />
+
+        {/* <Route path="*" element={<ErrorPage />} /> */}
+
+        {/* <Route
+          path="cart"
+          element={<Cart liked={liked} setLiked={setLiked} />}
+        /> */}
+
+        {/* <Route
+          path="wishlist"
+          element={
+            <WishList />
+          }
+        /> */}
+
+        {/* <Route path="product-detail" element={<ProductDetail />} />
+
+        <Route path="checkout" element={<Checkout />} />
+
+        <Route path="contact" element={<Contact />} />
+
+        <Route path="user" element={<User />} />
+
+        <Route path="about" element={<About />} /> */}
+
+        {/* <Route
+          path="shop"
+          element={
+            <Posts
+              liked={liked}
+              setLiked={setLiked}
+              trending={trending}
+              mCloth={mCloth}
+              wCloth={wCloth}
+            />
+          }
+        /> */}
+
+        {/* <Route path="signup" element={<Signup />} />
+
+        <Route path="login" element={<Login />} />
+
+        <Route path="shop" element={<ShopLayout />}>
+          <Route
+            index
+            element={
+              <Posts
+                liked={liked}
+                setLiked={setLiked}
+                trending={trending}
+                mCloth={mCloth}
+                wCloth={wCloth}
+              />
+            }
+          /> */}
+          {/* <Route
+            path=":name"
+            element={<ProductDetail liked={liked} setLiked={setLiked} />}
+          />
+        </Route> */}
+      </Route>
+    )
   );
+
+  return <RouterProvider router={main} />;
 }
 
 export default App;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>This is the header for the project</div>
+  )
+}
+
+export default Header
