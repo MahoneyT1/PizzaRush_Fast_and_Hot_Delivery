9de1299972ce39679407612ae276c7f4e7f5b990
Olawoyin <yustee2017@gmail.com>
2024-12-18 09:11:57 +0100

    Created the Hero , Menu , Testimonial Components

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='py-4'>
+        <div className="container p-0">
+            <h2 className='text-center menu fw-bold'>Our Menu</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+        <h3 className='text-center'>TESTIMONIALS</h3>
+        <p className='text-center'>What Are They Saying About Us</p>
+        <div className="container2">
+            <div className="testimonials">
+                <div className="testi-card row m-0">
+                    <div className="test-p">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima doloremque possimus velit. Totam velit similique rerum blanditiis, placeat eveniet veritatis.</p>
+                        <p className="lead">Olawoyin Gbolahan</p>
+                    </div>
+                    <div className="testi-i">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
